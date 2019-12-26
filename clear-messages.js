c9222f350f5830f56011099133aa5aab2e14e0f1
NightScript <maorninja@outlook.com>
2019-12-26 09:25:15 -0800

    Do one final fix before deleting this fork

--- clear-messages.js
+++ clear-messages.js
@@ -23,20 +23,20 @@ bot.on('message', async (message) => {
     //    1. Check if the user has enough permissions
     //    2. Check if I have the permission to execute the command
 
-    if (!message.channel.permissionsFor(message.author).hasPermission("MANAGE_MESSAGES"))
-      return message.channel.send("Sorry, you don't have the permission to execute the command \""+message.content+"\"");
-    else if (!message.channel.permissionsFor(bot.user).hasPermission("MANAGE_MESSAGES"))
-      return message.channel.send("Sorry, I don't have the permission to execute the command \""+message.content+"\"");
-
     // Only delete messages if the channel type is TextChannel
     // DO NOT delete messages in DM Channel or Group DM Channel
-    if (!message.guild)
+    if (!message.guild || message.author.bot)
       return;
+
+    if (!message.channel.permissionsFor(message.author).hasPermission("MANAGE_MESSAGES"))
+      return message.channel.send("Sorry, you don't have the permission to execute the command \""+CLEAR_MESSAGES+"\"");
+    else if (!message.channel.permissionsFor(bot.user).hasPermission("MANAGE_MESSAGES"))
+      return message.channel.send("Sorry, I don't have the permission to execute the command \""+CLEAR_MESSAGES+"\"");
 
     let messages = await message.channel.fetchMessages()
     try {
       message.channel.bulkDelete(messages);
-      let messagesDeleted = messages.array().length; // number of messages deleted
+      let messagesDeleted = messages.size; // number of messages deleted
 
       // Logging the number of messages deleted on both the channel and console.
       message.channel.send("Deletion of messages successful. Total messages deleted: "+messagesDeleted);
